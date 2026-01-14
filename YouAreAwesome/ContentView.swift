775a9920ac4c61f578617361a59d44de4ba55545
Jake Abramson <abramso@bc.edu>
2026-01-14 17:00:20 -0500

    Added Images from lesson

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,33 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I am a Programmer!"
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
         
         VStack {
             Spacer()
             
-            Image(systemName: "swift")
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
             
             Spacer()
-            HStack{
-                Button("Awesome!") {
-                    message = "Awesome!"
-                }
-                Button("Great!"){
-                    message = "Great!"
-                }
+        
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                message = (message == message1 ? message2: message1)
+                imageName = (imageName == "image0" ? "image1" : "image0")
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
         }
         .padding()
         
