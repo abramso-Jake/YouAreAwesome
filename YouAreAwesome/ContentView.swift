123a1b9731f18be0ed7cb4544b67d168048acae0
Jake Abramson <abramso@bc.edu>
2026-01-19 18:59:22 -0500

    Animation lesson 1.10

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,29 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
+    @State private var messageNum = 0
     @State private var imageName = ""
     @State private var imageNumber = 0
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration:0.15), value:imageName)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            
+                .animation(.default, value:imageName)
             Spacer()
-        
             Button("Show Message") {
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
-                message = (message == message1 ? message2: message1)
+                let messages = ["You are Awesome!", "When the Genius Bar Needs Help, They Call You!", "You are Great!", "Fabulous? That's You", "Keep it up!", "You Make Me Smile"]
+                message = messages[messageNum]
+                messageNum += 1
+                if messageNum == messages.count{
+                    messageNum = 0
+                }
                 imageName = "image\(imageNumber)"
                 imageNumber += 1
                 if imageNumber > 9 {
